757fb3461b141aa268592039df80cd4ba1d95cee
sefadalklc <sefadalk11@gmail.com>
2022-01-20 18:07:53 +0300

    login ve register sayfa geçişleri yapıldı

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="uyeOl">
+<h4>Giriş Ekranı</h4>
+
+<input type="text" placeholder="Email giriniz" v-model="email"/>
+<input type="password" placeholder="Parola giriniz" v-model="parola"/>
+<button>Giriş </button>
+  </form>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+    setup(){
+        
+        const email=ref('')
+        const parola=ref('')
+
+        const uyeOl=()=>{
+            console.log(email.value,parola.value);
+        }
+
+        return {email,parola,uyeOl}
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="home container">
     <h2>Chat App</h2>
-    <Register />
+    <div v-if="uyeMi">
+      <Login />
+      <p>Üye Olmak için <span @click="uyeMi=false" >Üye Ol</span> sayfasına gidiniz</p>
+    </div>
+    <div v-else>
+      <Register />
+      <p>Griş Yapmak İçin <span @click="uyeMi=true" >Giriş</span> sayfasına gidiniz</p>
+    </div>
   </div>
 </template>
 
 <script>
-import Register from '../components/Register.vue'
+import Register from '../components/Register.vue';
+import Login from '../components/Login.vue';
+import {ref} from 'vue'
+
 
 export default {
   name: 'Home',
   components:{
+    Login,
     Register
+  },
+  
+  setup(){
+    const uyeMi=ref(true)
+    return{uyeMi}
   }
 }
 </script>
