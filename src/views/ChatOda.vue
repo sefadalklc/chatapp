9e9902477f9c82c0031888c339c8c1f1213b8423
sefadalklc <sefadalk11@gmail.com>
2022-01-25 17:03:51 +0300

    chat odası navbar oluşturuldu çıkış işlemi gerçekleştirme işlemi yapıldı

--- src/views/ChatOda.vue
+++ src/views/ChatOda.vue
@@ -1,10 +1,13 @@
 <template>
-  <h3>Chat Odası</h3>
+  <div class="container">
+    <Navbar />
+  </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar.vue'
 export default {
-
+  components:{Navbar}
 }
 </script>
 
